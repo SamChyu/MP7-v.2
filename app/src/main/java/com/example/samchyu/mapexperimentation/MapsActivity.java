0ddbd83e3b402a704e1038702ea7c791f9b63d85
Michael Vitucci <michaelvitucci1@gmail.com>
2018-04-30 20:35:44 -0500

    Added location services and changed other things

--- app/src/main/java/com/example/samchyu/mapexperimentation/MapsActivity.java
+++ app/src/main/java/com/example/samchyu/mapexperimentation/MapsActivity.java
@@ -1,11 +1,25 @@
 package com.example.samchyu.mapexperimentation;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.android.volley.Request;
@@ -14,6 +28,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +39,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -50,6 +68,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public static List<Marker> stopMarkers = new ArrayList<>();
     public static List<Marker> poiMarkers = new ArrayList<>();
 
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private boolean locationPremission = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,13 +82,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getStops();
         getPOI();
 
+        getLocationPermissions();
+        getLocation();
+
+        // Create spinner (dropdown menu) for the map type
+        Spinner mapTypes = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mapTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mapTypes.setAdapter(adapter);
+        mapTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                setMapType(position);
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        parent.getItemAtPosition(position) + " selected",
+                        Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         Button location = findViewById(R.id.yourLocation);
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LatLng me = new LatLng(40.116, -88.2073);
-                poiMarkers.add(mMap.addMarker(new MarkerOptions().position(me).title("YOU ARE HERE")));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 15));
+                getLocation();
 
             }
         });
@@ -92,10 +138,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (isChecked) {
                     for (int i = 0; i < poiMarkers.size(); i++) {
                         poiMarkers.get(i).setVisible(true);
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(14f));
                     }
                 } else {
                     for (int i = 0; i < poiMarkers.size();i++) {
                         poiMarkers.get(i).setVisible(false);
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(17f));
                     }
                 }
             }
@@ -106,16 +154,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng urbana = new LatLng(40.116, -88.2073);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(urbana, 15));
+        getLocation();
     }
+
     void getStops() {
         String url = "https://developer.cumtd.com/api/v2.2/json/getstops?key=a13800cdc807401b9fcc4abbda713477";
         try {
@@ -228,7 +275,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             double lat = Double.valueOf(aStopData[1]);
             double lon = Double.valueOf(aStopData[2]);
             LatLng individualLatLng = new LatLng(lat, lon);
-            stopMarkers.add(mMap.addMarker(new MarkerOptions().position(individualLatLng).title(aStopData[0]).snippet(aStopData[3]).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).visible(false)));
+            stopMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .position(individualLatLng)
+                    .title(aStopData[0])
+                    .snippet(aStopData[3])
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .visible(false)));
         }
     }
     void createPOIMarkers(){
@@ -239,12 +291,105 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             double lat = Double.valueOf(aPoiData[1]);
             double lon = Double.valueOf(aPoiData[2]);
             LatLng individualLatLng = new LatLng(lat, lon);
-            poiMarkers.add(mMap.addMarker(new MarkerOptions().position(individualLatLng).title(aPoiData[0]).snippet(aPoiData[3]).visible(false)));
+            poiMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .position(individualLatLng)
+                    .title(aPoiData[0])
+                    .snippet(aPoiData[3])
+                    .visible(false)));
         }
 
     }
 
 
+    private void getLocationPermissions () {
+        Log.d(TAG, "getting permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION};
 
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == (PackageManager.PERMISSION_GRANTED)){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) == (PackageManager.PERMISSION_GRANTED)) {
+                locationPremission = true;
+            } else {
+                ActivityCompat.requestPermissions(this, permissions,1);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions,1);
+        }
+    }
 
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        locationPremission = false;
+        switch (requestCode) {
+            case 1: {
+                if(grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            locationPremission = false;
+                            return;
+                        }
+                    }
+                    locationPremission = true;
+
+                }
+            }
+        }
+    }
+
+    private void getLocation () {
+        Log.d(TAG, "Getting your location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (locationPremission) {
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Location found");
+                            Location currentLocation = (Location) task.getResult();
+                            LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                            mMap.setMyLocationEnabled(true);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17f));
+                        } else {
+                            Log.d(TAG, "Location not found");
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.d(TAG, "Did not have permission");
+        }
+    }
+
+    private void setMapType (int type) {
+        if (type == 1) {
+            mMap.setMapType(2);
+        } else if (type == 2) {
+            mMap.setMapType(3);
+        } else {
+            mMap.setMapType(1);
+        }
+    }
 }
